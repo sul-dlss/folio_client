70e2a60f93417bb63355acaf27b2d3f9f977e2d1
Peter Mangiafico <peter@mangiafico.org>
2023-02-17 16:05:25 -0800

    fixes

--- lib/folio_client/token_wrapper.rb
+++ lib/folio_client/token_wrapper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-require "byebug"
 class FolioClient
   # Wraps API operations to request new access token if expired
   class TokenWrapper
-    def self.refresh(config, connection, &block)
+    def self.refresh(config, connection)
       yield
     rescue UnexpectedResponse::UnauthorizedError
       config.token = Authenticator.token(config.login_params, connection)
