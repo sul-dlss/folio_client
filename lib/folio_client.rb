4856153c8b12a701bbed252c1ecaf59ef02411d2
Johnathan Martin <john.martin@stanford.edu>
2023-09-15 15:57:56 -0700

    collapse TokenWrapper helper class into FolioClient
    remove some indirection since it's just one short method and since FolioClient is the only user of TokenWrapper.  this also allows us to get rid of the params on the refresh method, since the desired objects passed as params are now available as ivars from the FolioClient instance.
    
    also, make the moved method a bit more verbose/explicit, and a little less terse, by removing some ruby cleverness.
    
    hopefully this leaves things a bit more intelligible at a glance.
    
    ref https://github.com/sul-dlss/folio_client/pull/77

--- lib/folio_client.rb
+++ lib/folio_client.rb
@@ -51,15 +51,15 @@ class FolioClient
     def configure(url:, login_params:, okapi_headers:, timeout: default_timeout)
       instance.config = OpenStruct.new(
         # For the initial token, use a dummy value to avoid hitting any APIs
-        # during configuration, allowing the `TokenWrapper` to handle auto-magic
-        # token refreshing. Why not immediately get a valid token? Our apps
+        # during configuration, allowing `with_token_refresh_when_unauthorized` to handle
+        # auto-magic token refreshing. Why not immediately get a valid token? Our apps
         # commonly invoke client `.configure` methods in the initializer in all
         # application environments, even those that are never expected to
         # connect to production APIs, such as local development machines.
         #
         # NOTE: `nil` and blank string cannot be used as dummy values here as
         # they lead to a malformed request to be sent, which triggers an
-        # exception not rescued by `TokenWrapper`
+        # exception not rescued by `with_token_refresh_when_unauthorized`
         token: "a temporary dummy token to avoid hitting the API before it is needed",
         url: url,
         login_params: login_params,
@@ -83,7 +83,7 @@ class FolioClient
   # @param path [String] the path to the Folio API request
   # @param params [Hash] params to get to the API
   def get(path, params = {})
-    response = TokenWrapper.refresh(config, connection) do
+    response = with_token_refresh_when_unauthorized do
       connection.get(path, params, {"x-okapi-token": config.token})
     end
 
@@ -100,7 +100,7 @@ class FolioClient
   # @param body [Object] body to post to the API as JSON
   def post(path, body = nil, content_type: "application/json")
     req_body = (content_type == "application/json") ? body&.to_json : body
-    response = TokenWrapper.refresh(config, connection) do
+    response = with_token_refresh_when_unauthorized do
       req_headers = {
         "x-okapi-token": config.token,
         "content-type": content_type
@@ -121,7 +121,7 @@ class FolioClient
   # @param body [Object] body to put to the API as JSON
   def put(path, body = nil, content_type: "application/json")
     req_body = (content_type == "application/json") ? body&.to_json : body
-    response = TokenWrapper.refresh(config, connection) do
+    response = with_token_refresh_when_unauthorized do
       req_headers = {
         "x-okapi-token": config.token,
         "content-type": content_type
@@ -226,5 +226,29 @@ class FolioClient
 
   def default_timeout
     120
+  end
+
+  private
+
+  # Wraps API operations to request new access token if expired.
+  # @yieldreturn response [Faraday::Response] the response to inspect
+  #
+  # @note You likely want to make sure you're wrapping a _single_ HTTP request in this
+  # method, because 1) all calls in the block will be retried from the top if there's
+  # an authN failure detected, and 2) only the response returned by the block will be
+  # inspected for authN failure.
+  # Related: consider that the client instance and its token will live across many
+  # invocations of the FolioClient methods once the client is configured by a consuming application,
+  # since this class is a Singleton.  Thus, a token may expire between any two calls (i.e. it
+  # isn't necessary for a set of operations to collectively take longer than the token lifetime for
+  # expiry to fall in the middle of that related set of HTTP calls).
+  def with_token_refresh_when_unauthorized
+    response = yield
+    UnexpectedResponse.call(response) unless response.success?
+
+    response
+  rescue UnauthorizedError
+    config.token = Authenticator.token(config.login_params, connection)
+    yield
   end
 end
